a311bf60a78f2a28e8182b618377647ceed59867
66golden <denis.951236874@mail.ru>
2022-12-19 12:44:02 +0300

    rewrite2

--- task_c.cpp
+++ task_c.cpp
@@ -2,90 +2,97 @@
 #include <cmath>
 #include <iostream>
 #include <vector>
-//
-struct Tree {
-  std::vector<int> arr;
-  int ans;
-  int value;
 
-  void Get(int index, std::vector<int> finds, int tl, int tr) {
-    if (tl == finds[0] && tr == finds[1]) {
-      if (arr[index] >= value) {
-        while (2 * index + 2 < int(arr.size())) {
-          if (arr[2 * index + 1] >= value) {
+struct Tree {
+ private:
+  std::vector<int> arr_;
+  int height_ = 0;
+  int ans_ = 0;
+  int value_ = 0;
+
+ public:
+  Tree(std::vector<int> arr_sup, int n) {
+    height_ = 0;
+    int size, tmp;
+    while (int(pow(2, height_)) < n) {
+      ++height_;
+    }
+    size = 2 * int(pow(2, height_)) - 1;
+    arr_.resize(size, 0);
+    tmp = int(pow(2, height_)) - 1;
+    for (int i = tmp; i < tmp + n; i++) {
+      arr_[i] = arr_sup[i - tmp];
+    }
+    for (int i = height_ - 1; i >= 0; i--) {
+      for (int l = int(pow(2, i)) - 1; l < int(pow(2, i + 1)) - 1; l++) {
+        arr_[l] = std::max(arr_[2 * l + 1], arr_[2 * l + 2]);
+      }
+    }
+  }
+
+  void Get(int index, int left_find, int right_find, int tl, int tr) {
+    if (tl == left_find && tr == right_find) {
+      if (arr_[index] >= value_) {
+        while (2 * index + 2 < int(arr_.size())) {
+          if (arr_[2 * index + 1] >= value_) {
             index = 2 * index + 1;
-          } else if (arr[2 * index + 2] >= value) {
+          } else if (arr_[2 * index + 2] >= value_) {
             index = 2 * index + 2;
           }
         }
-        ans = std::min(ans, index);
+        ans_ = std::min(ans_, index);
       }
       return;
     }
     int tm = (tl + tr) / 2;
-    if (finds[0] < tm) {
-      std::vector<int> help1 = {finds[0], std::min(tm, finds[1])};
-      Get(2 * index + 1, help1, tl, tm);
+    if (left_find < tm) {
+      Get(2 * index + 1, left_find, std::min(tm, right_find), tl, tm);
     }
-    if (finds[1] > tm) {
-      std::vector<int> help2 = {std::max(tm, finds[0]), finds[1]};
-      Get(2 * index + 2, help2, tm, tr);
+    if (right_find > tm) {
+      Get(2 * index + 2, std::max(tm, left_find), right_find, tm, tr);
     }
   }
 
-  void Set(int index, int value) {
-    arr[index] = value;
+  void Set(int index, int key) {
+    arr_[index] = key;
     while (index > 0) {
       index = (index - 1) / 2;
-      arr[index] = std::max(arr[2 * index + 1], arr[2 * index + 2]);
+      arr_[index] = std::max(arr_[2 * index + 1], arr_[2 * index + 2]);
     }
   }
-};
 
-void Solve(Tree& tree, int m, int k, int n) {
-  int tmp, i, j;
-  for (int p = 0; p < m; p++) {
-    std::cin >> tmp >> i >> j;
-    if (tmp == 0) {
-      tree.Set(int(pow(2, k)) + i - 2, j);
-    } else {
-      tree.ans = int(tree.arr.size());
-      tree.value = j;
-      std::vector<int> finds = {i - 1, n};
-      tree.Get(0, finds, 0, int(pow(2, k)));
-      if (tree.ans == int(tree.arr.size())) {
-        std::cout << "-1" << '\n';
-      } else {
-        std::cout << tree.ans - int(pow(2, k)) + 2 << '\n';
-      }
-    }
-  }
-}
+  friend int main();
+};
 
 int main() {
   std::ios::sync_with_stdio(false);
   std::cin.tie(nullptr);
 
-  int n, m, k = 0, size, tmp;
+  int n, m;
   std::cin >> n >> m;
-
-  while (int(pow(2, k)) < n) {
-    k++;
+  std::vector<int> value(n);
+  for (int i = 0; i < n; ++i) {
+    std::cin >> value[i];
   }
-  size = 2 * int(pow(2, k)) - 1;
-  Tree tree;
-  tree.arr.resize(size, 0);
 
-  tmp = int(pow(2, k)) - 1;
-  for (int i = tmp; i < tmp + n; i++) {
-    std::cin >> tree.arr[i];
-  }
-  for (int i = k - 1; i >= 0; i--) {
-    for (int l = int(pow(2, i)) - 1; l < int(pow(2, i + 1)) - 1; l++) {
-      tree.arr[l] = std::max(tree.arr[2 * l + 1], tree.arr[2 * l + 2]);
+  Tree tree(value, n);
+
+  int tmp, i, j;
+  for (int p = 0; p < m; p++) {
+    std::cin >> tmp >> i >> j;
+    if (tmp == 0) {
+      tree.Set(int(pow(2, tree.height_)) + i - 2, j);
+    } else {
+      tree.ans_ = int(tree.arr_.size());
+      tree.value_ = j;
+      tree.Get(0, i - 1, n, 0, int(pow(2, tree.height_)));
+      if (tree.ans_ == int(tree.arr_.size())) {
+        std::cout << "-1" << '\n';
+      } else {
+        std::cout << tree.ans_ - int(pow(2, tree.height_)) + 2 << '\n';
+      }
     }
   }
 
-  Solve(tree, m, k, n);
   return 0;
 }
