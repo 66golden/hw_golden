fbea692afdc62b881f7e4e2d04c02ac2dbbe7811
Golovko Denis <golovko.ds@phystech.edu>
2023-05-15 15:41:37 +0300

    Update taskB.cpp

--- taskB.cpp
+++ taskB.cpp
@@ -2,18 +2,11 @@
 #include <iostream>
 #include <vector>
 
-int main() {
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(nullptr);
-  int n;
-  const long long kInfty = 10000000000;
-  std::cin >> n;
-  std::vector<long long> numbers(n);
-  for (int i = 0; i < n; i++) {
-    std::cin >> numbers[i];
-  }
+const long long kInf = 10000000000;
+
+void Solve(std::vector<long long>& numbers, int n) {
   std::reverse(numbers.begin(), numbers.end());
-  std::vector<std::pair<long long, int>> dp(n, std::make_pair(kInfty, -1));
+  std::vector<std::pair<long long, int>> dp(n, std::make_pair(kInf, -1));
   std::vector<int> pos(n);
   for (int i = 0; i < n; i++) {
     auto low =
@@ -23,7 +16,7 @@ int main() {
     pos[i] = (id_x == 0) ? -1 : dp[id_x - 1].second;
   }
   int i = n - 1;
-  while (dp[i].first == kInfty) {
+  while (dp[i].first == kInf) {
     i--;
   }
   std::cout << i + 1 << '\n';
@@ -36,4 +29,16 @@ int main() {
   for (int i = 0; i < int(ans.size()); i++) {
     std::cout << (n - ans[i]) << ' ';
   }
+}
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  int n;
+  std::cin >> n;
+  std::vector<long long> numbers(n);
+  for (int i = 0; i < n; i++) {
+    std::cin >> numbers[i];
+  }
+  Solve(numbers, n);
 }
