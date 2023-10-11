d362318d04d852fad698c99d0cb45acaaba3d8a5
Golovko Denis <golovko.ds@phystech.edu>
2023-10-11 22:33:43 +0300

    Update 10_E.cpp

--- 10_E.cpp
+++ 10_E.cpp
@@ -4,20 +4,23 @@
 #include <unordered_map>
 #include <vector>
 
-struct Node {
-  std::vector<int> arr;
-  bool term;
-  int height;
-  int subtree_term = 0;
-
-  Node(int x = 0) : arr(10, -1), term(false), height(x) {}
-};
+const int kNodeNull = -1;
+const int kNodeSize = 10;
 
 class Trie {
  private:
+  struct Node {
+    std::vector<int> to;
+    bool term;
+    int height;
+    int subtree_term = 0;
+
+    Node(int x = 0) : to(kNodeSize, kNodeNull), term(false), height(x) {}
+  };
+
   std::vector<Node> trie_;
   std::vector<bool> visited_;
-  std::unordered_map<int, int> um_;
+  std::unordered_map<int, int> ans_by_heights_;
 
  public:
   Trie() = default;
@@ -28,52 +31,53 @@ class Trie {
     }
     int cur_vert = 0;
     int cur_height = 0;
-    for (char ii : str) {
-      if (trie_[cur_vert].arr[ii - '0'] == -1) {
-        trie_[cur_vert].arr[ii - '0'] = int(trie_.size());
+    for (char c : str) {
+      if (trie_[cur_vert].to[c - '0'] == kNodeNull) {
+        trie_[cur_vert].to[c - '0'] = static_cast<int>(trie_.size());
         trie_.emplace_back(cur_height + 1);
       }
-      cur_vert = trie_[cur_vert].arr[ii - '0'];
+      cur_vert = trie_[cur_vert].to[c - '0'];
       ++cur_height;
     }
     trie_[cur_vert].term = true;
   }
 
-  void Dfs(int cur_vert, int max_height, int kk) {
+  void Dfs(int cur_vert, int max_height, int fake_size) {
     visited_[cur_vert] = true;
     if (trie_[cur_vert].term) {
       trie_[cur_vert].subtree_term += 1;
     }
-    for (int ii = 0; ii < 10; ii++) {
-      int next_vert = trie_[cur_vert].arr[ii];
-      if (next_vert != -1) {
+    for (int i = 0; i < kNodeSize; i++) {
+      int next_vert = trie_[cur_vert].to[i];
+      if (next_vert != kNodeNull) {
         if (!visited_[next_vert]) {
-          Dfs(next_vert, max_height, kk);
+          Dfs(next_vert, max_height, fake_size);
         }
         trie_[cur_vert].subtree_term += trie_[next_vert].subtree_term;
       }
     }
-    if (trie_[cur_vert].subtree_term >= kk) {
-      um_[trie_[cur_vert].height] += 1;
+    if (trie_[cur_vert].subtree_term >= fake_size) {
+      ans_by_heights_[trie_[cur_vert].height] += 1;
     }
   }
 
-  void Solve(const std::vector<int>& arr_ll, int max_height, int kk) {
+  void Solve(const std::vector<int>& request_array, int max_height,
+             int fake_size) {
     visited_.assign(trie_.size(), false);
-    Dfs(0, max_height, kk);
-    for (int height : arr_ll) {
-      std::cout << um_[height] << '\n';
+    Dfs(0, max_height, fake_size);
+    for (int height : request_array) {
+      std::cout << ans_by_heights_[height] << '\n';
     }
   }
 };
 
 std::string MakeGood(const std::string& str) {
   std::string ans;
-  int nn = int(str.length());
-  ans.reserve(nn * 2);
-  for (int ii = 0; ii < nn; ii++) {
-    ans += str[ii];
-    ans += str[nn - ii - 1];
+  int len = static_cast<int>(str.length());
+  ans.reserve(len * 2);
+  for (int i = 0; i < len; i++) {
+    ans += str[i];
+    ans += str[len - i - 1];
   }
   return ans;
 }
@@ -82,28 +86,29 @@ int main() {
   std::ios::sync_with_stdio(false);
   std::cin.tie(nullptr);
   std::cout.tie(nullptr);
-  int nn, kk, qq, ll;
-  std::cin >> nn >> kk;
-  std::vector<std::string> arr;
+  int num_of_people, fake_size, num_of_requests, num_of_identical_letters;
+  std::cin >> num_of_people >> fake_size;
+  std::vector<std::string> words;
 
   std::string tmp;
-  for (int ii = 0; ii < nn; ++ii) {
+  for (int i = 0; i < num_of_people; ++i) {
     std::cin >> tmp;
-    arr.push_back(MakeGood(tmp));
+    words.push_back(MakeGood(tmp));
   }
 
   Trie trie;
-  for (const auto& word : arr) {
+  for (const auto& word : words) {
     trie.AddWord(word);
   }
 
-  std::cin >> qq;
-  std::vector<int> arr_ll(qq);
-  int max_l = 0;
-  for (int ii = 0; ii < qq; ++ii) {
-    std::cin >> ll;
-    arr_ll[ii] = ll * 2;
-    max_l = std::max(max_l, arr_ll[ii]);
+  std::cin >> num_of_requests;
+  std::vector<int> request_array(num_of_requests);
+  int max_num_of_identical_letters = 0;
+  for (int i = 0; i < num_of_requests; ++i) {
+    std::cin >> num_of_identical_letters;
+    request_array[i] = num_of_identical_letters * 2;
+    max_num_of_identical_letters =
+        std::max(max_num_of_identical_letters, request_array[i]);
   }
-  trie.Solve(arr_ll, max_l, kk);
+  trie.Solve(request_array, max_num_of_identical_letters, fake_size);
 }
