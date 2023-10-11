ce90eb8262ec19f4da1051d96f770ea127148238
Golovko Denis <golovko.ds@phystech.edu>
2023-10-11 21:34:59 +0300

    Update 10_C.cpp

--- 10_C.cpp
+++ 10_C.cpp
@@ -4,45 +4,47 @@
 #include <unordered_map>
 #include <vector>
 
+const int kLinkNull = -1;
+
 struct Node {
   std::unordered_map<char, int> next;
-  int fail = -1;
+  int fail = kLinkNull;
   std::vector<int> output;
 };
 
 int BuildTrie(const std::vector<std::string>& words, std::vector<Node>& nodes) {
   nodes.push_back(Node());
   int root = 0;
-  for (int ii = 0; ii < int(words.size()); ++ii) {
-    const std::string& word = words[ii];
+  for (size_t i = 0; i < words.size(); ++i) {
+    const std::string& word = words[i];
     int node = root;
-    for (char cc : word) {
-      if (nodes[node].next.count(cc) == 0) {
+    for (char c : word) {
+      if (nodes[node].next.count(c) == 0) {
         nodes.push_back(Node());
-        nodes[node].next[cc] = nodes.size() - 1;
+        nodes[node].next[c] = nodes.size() - 1;
       }
-      node = nodes[node].next[cc];
+      node = nodes[node].next[c];
     }
-    nodes[node].output.push_back(ii);
+    nodes[node].output.push_back(i);
   }
-  std::queue<int> qq;
+  std::queue<int> queue;
   for (auto & [ ch, child ] : nodes[root].next) {
     nodes[child].fail = root;
-    qq.push(child);
+    queue.push(child);
   }
-  while (!qq.empty()) {
-    int cur = qq.front();
-    qq.pop();
+  while (!queue.empty()) {
+    int cur = queue.front();
+    queue.pop();
     for (auto & [ ch, child ] : nodes[cur].next) {
       int fail = nodes[cur].fail;
-      while (fail != -1 && nodes[fail].next.count(ch) == 0) {
+      while (fail != kLinkNull && nodes[fail].next.count(ch) == 0) {
         fail = nodes[fail].fail;
       }
-      nodes[child].fail = fail != -1 ? nodes[fail].next[ch] : root;
+      nodes[child].fail = fail != kLinkNull ? nodes[fail].next[ch] : root;
       for (int id : nodes[nodes[child].fail].output) {
         nodes[child].output.push_back(id);
       }
-      qq.push(child);
+      queue.push(child);
     }
   }
   return root;
@@ -53,23 +55,24 @@ void SearchWords(int root, const std::string& text,
                  const std::vector<Node>& nodes, int num) {
   std::vector<std::vector<int>> ans(num);
   int node = root;
-  for (int ii = 0; ii < int(text.size()); ++ii) {
-    char cc = text[ii];
-    while (node != -1 && nodes[node].next.count(cc) == 0) {
+  for (int i = 0; i < int(text.size()); ++i) {
+    char c = text[i];
+    while (node != kLinkNull && nodes[node].next.count(c) == 0) {
       node = nodes[node].fail;
     }
-    node = node != -1 ? (nodes[node].next.find(cc) != nodes[node].next.end()
-                             ? nodes[node].next.at(cc)
-                             : -1)
-                      : root;
+    node = node != kLinkNull
+               ? (nodes[node].next.find(c) != nodes[node].next.end()
+                      ? nodes[node].next.at(c)
+                      : kLinkNull)
+               : root;
     for (int id : nodes[node].output) {
-      ans[id].push_back(ii - words[id].size() + 1);
+      ans[id].push_back(i - words[id].size() + 1);
     }
   }
-  for (int ii = 0; ii < num; ++ii) {
-    std::cout << ans[ii].size() << ' ';
-    for (int jj : ans[ii]) {
-      std::cout << jj + 1 << ' ';
+  for (int i = 0; i < num; ++i) {
+    std::cout << ans[i].size() << ' ';
+    for (int j : ans[i]) {
+      std::cout << j + 1 << ' ';
     }
     std::cout << '\n';
   }
@@ -82,8 +85,8 @@ int main() {
   std::cin >> num;
   std::vector<std::string> words(num);
   std::vector<Node> nodes;
-  for (int ii = 0; ii < num; ++ii) {
-    std::cin >> words[ii];
+  for (int i = 0; i < num; ++i) {
+    std::cin >> words[i];
   }
   int root = BuildTrie(words, nodes);
   SearchWords(root, text, words, nodes, num);
