f205bc43ebf2a1294c3684a236ab8f166c564ba3
golden_66 <golovko.ds@phystech.edu>
2023-11-04 00:50:52 +0300

    changes task_d

--- task_d.cpp
+++ task_d.cpp
@@ -2,25 +2,24 @@
 #include <iostream>
 #include <vector>
 
-
 std::vector<int> CreateSuffixArray(std::string& str) {
   int str_length = static_cast<int>(str.length());
   std::vector<int> suffix_array(str_length);
   std::vector<int> equivalence_classes(str_length);
-  std::vector<std::pair<char, int>> sorted_simbols(str_length);
+  std::vector<std::pair<char, int>> sorted_symbols(str_length);
   for (int i = 0; i < str_length; i++) {
-    sorted_simbols[i] = {str[i], i};
+    sorted_symbols[i] = {str[i], i};
   }
-  sort(sorted_simbols.begin(), sorted_simbols.end());
+  sort(sorted_symbols.begin(), sorted_symbols.end());
   for (int i = 0; i < str_length; i++) {
-    suffix_array[i] = sorted_simbols[i].second;
+    suffix_array[i] = sorted_symbols[i].second;
   }
   equivalence_classes[suffix_array[0]] = 0;
   for (int i = 1; i < str_length; i++) {
     equivalence_classes[suffix_array[i]] =
         equivalence_classes[suffix_array[i - 1]] +
-        static_cast<int>(sorted_simbols[i].first !=
-                         sorted_simbols[i - 1].first);
+        static_cast<int>(sorted_symbols[i].first !=
+                         sorted_symbols[i - 1].first);
   }
   int iter = 0;
   while ((1 << iter) < str_length) {
@@ -45,10 +44,7 @@ std::vector<int> CreateSuffixArray(std::string& str) {
   return suffix_array;
 }
 
-int main() {
-  std::string str;
-  std::cin >> str;
-  std::vector<int> suffix_array = CreateSuffixArray(str);
+void SuffixArrayPrint(std::vector<int>& suffix_array, std::string& str) {
   std::string result;
   for (int pos : suffix_array) {
     if (pos != 0) {
@@ -58,4 +54,11 @@ int main() {
     }
   }
   std::cout << result;
+}
+
+int main() {
+  std::string str;
+  std::cin >> str;
+  std::vector<int> suffix_array = CreateSuffixArray(str);
+  SuffixArrayPrint(suffix_array, str);
 }
